e75244987b091e57fbee401218d9e509cc69e8e1
Matheus Filipe da Silva Gomes <matheusg@motorola.com>
2024-08-14 22:05:19 -0300

    Fix background colors
    - Sometimes, all background get white, just fixed it
    -

--- termo.py
+++ termo.py
@@ -5,37 +5,46 @@ from colorama import Back, Style
 with open('some_words.txt', 'r', encoding='utf-8') as arquivo:
     lista_palavras = arquivo.readlines()
 
-print(Style.RESET_ALL)
 palavra_secreta = rand.choice(lista_palavras).upper()
 tamanho_palavra = len(palavra_secreta) - 1
 palavra_user = '*' * tamanho_palavra
-tentativas = 0
+tentativas = 1
 palavra=''
 
+
+# print(palavra_secreta)
 while palavra != palavra_user:
-    if tentativas == 5:
-        break
-    tentativas += 1
+
     listado_palavra_user = list(palavra_user)
     print(f'\n{tentativas}x. Palavra Secreta: {palavra_user}')
 
     palavra = input('Digite uma palavra de 5 letras: ').upper()
     contador = 0
+    if len(palavra) >= 6 or len(palavra) <5:
+        print('\nDIGITE APENAS 5 LETRAS CABEÇA DE PIKA!!!\n')
+        continue
 
+    tentativas += 1
     for i in palavra:
         if i in palavra_secreta:
             if palavra_secreta[contador] == i:
                 listado_palavra_user[contador] = i
                 palavra_user = ''.join(listado_palavra_user)
                 print(Back.GREEN, colored(f'{i} ', 'green'), end='')
+                print(Style.RESET_ALL, end='')
             else:
-
                 print(Back.YELLOW, colored(f'{i} ', 'yellow'),end='')
+                print(Style.RESET_ALL, end='')
         else:
             print(Back.WHITE, f'{i} ', end='')
+            print(Style.RESET_ALL, end='')
         contador += 1
 
-if tentativas == 5:
-    print('\nPerdeu otário. A Palavra era', palavra_secreta)
-else:
-    print(f'\nPARABÉNS, VOCÊ DESCOBRIU A PALAVRA SECRETA COM {tentativas} TENTATIVAS')
+    if tentativas == 6:
+        print('\nPerdeu. A Palavra era', palavra_secreta)
+        break
+    elif tentativas < 6:
+        continue
+    elif palavra == palavra_user:
+        print(f'\nPARABÉNS, VOCÊ DESCOBRIU A PALAVRA SECRETA COM {tentativas} TENTATIVAS')
+        break
